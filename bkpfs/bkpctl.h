76d0547edb63cd80ec4486ad8a95fa71f5a69312
Kaustav Sarkar <kaustav.manutdforlife@gmail.com>
2019-07-05 18:45:25 +0530

    Deleted Files

--- bkpfs/bkpctl.h
+++
@@ -1,8 +0,0 @@
-#ifndef BKPCTL_H
-#define BKPCTL_H
-
-#define BKPFS_LIST_VERSIONS _IOR(0x15,0,char*)
-#define BKPFS_REMOVE_VERSIONS _IOWR(0x20,1,char*)
-#define BKPFS_VIEW_VERSION _IOR(0x14,2,char*)
-#define BKPFS_RESTORE_VERSION _IOWR(0x21,3,char*)
-#endif
